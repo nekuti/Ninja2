9b77faa08b89fe88645ec6cdc78a7e87b4ba3994
nekuti <henohenomoezi@gmail.com>
2017-12-04 18:30:24 +0900

    モデルのテスト(6回目)

---
+++ Assets/Ninja/Scripts/Ando/Skybox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skybox : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //  メッシュフィルターの取得
+        MeshFilter filter = GetComponent<MeshFilter>();
+        //  メッシュを保存
+        Mesh mesh = filter.mesh;
+
+        //  サブメッシュの数だけ回す
+        for (int i = 0; i < mesh.subMeshCount; i++)
+        {
+            //  ポリゴンのインデックスを取得する
+            int[] triangles = mesh.GetTriangles(i);
+
+            //  三角形なので３つずつ足していく
+            for(int j = 0; j < triangles.Length; j += 3)
+            {
+                //  ポリゴンを反転させる
+                int index = triangles[j + 1];
+                triangles[j + 1] = triangles[j + 2];
+                triangles[j + 2] = index;
+            }
+            //  反転したメッシュを設定する
+            mesh.SetTriangles(triangles, i);
+        }
+    }
+}
