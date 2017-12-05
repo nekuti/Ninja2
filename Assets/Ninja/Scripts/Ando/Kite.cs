ac2fffec9003662898c1ffdbee0e988c802a234e
nekuti <henohenomoezi@gmail.com>
2017-12-05 16:45:51 +0900

    凧にHIT判定を追加　凧によって処理を分岐

---
+++ Assets/Ninja/Scripts/Ando/Kite.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ando
+{
+    public class Kite : MonoBehaviour
+    {
+        //  凧の種類
+        public KiteType myType = KiteType.None;
+        
+        //  凧に苦無が当たったか
+        public bool hit = false;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if(collision.gameObject.tag = TagName.Attack)
+            {
+                hit = true;
+            }
+        }
+
+    }
+}
---
+++ Assets/Ninja/Scripts/Ando/TitleManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ando
+{
+    //  凧の種類
+    public enum KiteType
+    {
+        None,
+        Start,
+        Tutorial,
+        End,
+    }
+
+    public class TitleManager : MonoBehaviour
+    {
+        //  凧の情報を格納
+        public List<Kite> kites;
+
+        //  シーン遷移のステート
+        private KiteType transitionState = KiteType.None;
+
+        // Use this for initialization
+        void Start()
+        {
+            transitionState = KiteType.None;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            foreach(Kite kete in kites)
+            {
+                //  シーン遷移のステートに情報が入っている場合はループを抜ける
+                if(transitionState != KiteType.None)
+                {
+                    break;
+                }
+
+                //  凧に当たったか確認
+                if (kete.hit)
+                {
+                    //  シーン遷移のステートに情報を入れる
+                    transitionState = kete.myType;
+                }
+            }
+
+            switch (transitionState)
+            {
+                case KiteType.Start:
+                    Debug.Log("ゲームを開始");
+                    break;
+                case KiteType.Tutorial:
+                    Debug.Log("ゲームをチュートリアル");
+                    break;
+                case KiteType.End:
+                    Debug.Log("ゲームを終了");
+                    break;
+            }
+
+        }
+
+    }
+}
