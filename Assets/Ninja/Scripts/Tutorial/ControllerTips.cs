853fab12e2e9d7cba238ceb4356a2fbb8130f185
Masakazu_Kondo <osiruko0510@gmail.com>
2017-12-05 17:07:35 +0900

    コントローラーのtransfomeを取得するscriptを追加

---
+++ Assets/Ninja/Scripts/Tutorial/ControllerTips.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Kojima;
+
+
+namespace Kondo
+{
+    public class ControllerTips : MonoBehaviour
+    {
+        public string displayText;
+        public int fontSize = 14;
+        public Transform drawLineFrom;
+        public Transform drawLineTo;
+        public float lineWidth = 0.001f;
+        public Color fontColor = Color.white;
+        public Color lineColor = Color.black;
+        public Color backGroundColor = Color.black;
+
+
+
+        public LineRenderer line;
+
+
+        // Use this for initialization
+        void Start()
+        {
+            //InputDevice.ClickDownTrriger(HandType.Left);
+
+            ResetTips();
+            //if(InputDevice.Press(ButtonType.Trigger, HandType.Left))
+            //{
+
+            //}
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            SetTransform();
+            DrawLine();
+        }
+
+
+        public void ResetTips()
+        {
+            SetText("FrontText");
+            SetText("BackText");
+            SetLine();
+            SetBackGround();
+        }
+
+
+        private void SetTransform()
+        {
+            if(drawLineTo == null )
+            {
+                drawLineTo = FindControllerParts.GetTransfomeParts(HandType.Left, PartsType.Trigger);
+
+            }
+        }
+
+
+        private void SetText(string name)
+        {
+            Text t = transform.Find("Canvas/" + name).GetComponent<Text>();
+            //t.material = Resources.Load("UIText") as Material;
+            t.text = displayText;
+            t.color = fontColor;
+            t.fontSize = fontSize;
+        }
+
+        private void SetBackGround()
+        {
+            Image backGroud = transform.Find("Canvas/BackGround").GetComponent<Image>();
+            backGroud.color = backGroundColor;
+        }
+
+
+        private void SetLine()
+        {
+            line = transform.Find("Line").GetComponent<LineRenderer>();
+            //line.material = Resources.Load("TooltipLine") as Material;
+            line.material.color = lineColor;
+            line.startColor = lineColor;
+            line.endColor = lineColor;
+            line.startWidth = lineWidth;
+            line.endWidth = lineWidth;
+
+            if (drawLineFrom == null)
+            {
+                drawLineFrom = transform;
+            }
+        }
+
+
+        private void DrawLine()
+        {
+            if (drawLineTo != null)
+            {
+                line.SetPosition(0, drawLineFrom.position);
+                line.SetPosition(1, drawLineTo.position);
+            }
+        }
+
+    }
+}
+
---
+++ Assets/Ninja/Scripts/Tutorial/FindControllerParts.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Kondo
+{
+    public enum PartsType
+    {
+        Base,
+        Button,
+        Lgrip,
+        Rgrip,
+        Trackpad,
+        Trigger,
+        MaxParts,
+    }
+
+    public enum HandType
+    {
+        Left,
+        Right
+    }
+
+
+
+    public class FindControllerParts : MonoBehaviour
+    {
+
+        // 手を指定
+        public HandType handType;
+
+        // Find用配列
+        private string[] partsName = 
+            {"base",
+             "button",
+             "lgrip",
+             "rgrip",
+             "trackpad",
+             "trigger"};
+
+
+        // 親を保存する
+        // コントローラパーツのtransform取得用
+        private static Transform[][] controller = new Transform[2][];
+
+        // Findが終了しているかどうか
+        private bool isFindEnd = false;
+
+
+        
+
+
+        // Use this for initialization
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(!isFindEnd)
+            {
+                FindAllParts(this.gameObject);
+            }
+        }
+
+
+
+        /// <summary>
+        /// アタッチされたオブジェクトからパーツを探す
+        /// </summary>
+        /// <param name="obj"></param>
+       private void FindAllParts(GameObject obj)
+        {
+            Transform parent = obj.transform;
+
+            // 子がいなければ処理を飛ばす
+            if (parent.childCount == 0)
+            {
+                Debug.Log(obj + "の子無いのでFindできません");
+                return;
+            }
+
+            // 指定された手の各パーツをFindしていく
+            for(int count = 0; count < (int)PartsType.MaxParts;count++)
+            {
+                controller[(int)handType][count] = parent.Find(partsName[count]).GetComponent<Transform>();
+
+                if (controller[(int)handType][count] == null)
+                {
+                    Debug.Log(partsName[count]+"をFindできませんでした。");
+                }
+            }
+
+            if(controller != null)
+            {
+                Debug.Log("Findが完了しました");
+                isFindEnd = true;
+            }
+
+        }
+
+        /// <summary>
+        /// 手とパーツを指定しそのパーツのtransfomeを取得
+        /// </summary>
+        /// <param name="aHand">どちらの手か</param>
+        /// <param name="aParts">どのパーツか</param>
+        /// <returns></returns>
+        public static Transform GetTransfomeParts(HandType aHand, PartsType aParts)
+        {
+            Transform t = controller[(int)aHand][(int)aParts];
+            if(t == null)
+            {
+                Debug.Log(aParts + "の取得に失敗しました。");
+                return null;
+            }
+            return t;
+        }
+
+
+    }
+
+}
